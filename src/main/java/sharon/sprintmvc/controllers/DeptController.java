261612e0a5f022294454b73e3e0b5bc2b39666d9
Ashniavo <sharonrazakarivelo@gmail.com>
2026-06-30 11:21:25 +0300

    Exemple avec meme URL en GET et POST sur des methodes differentes

--- src/main/java/sharon/sprintmvc/controllers/DeptController.java
+++ src/main/java/sharon/sprintmvc/controllers/DeptController.java
@@ -6,13 +6,18 @@ import sharon.sprintmvc.annotation.URLMapping;
 @Controller("DeptController")
 public class DeptController {
 
-    @URLMapping("/dept/new")
+    @URLMapping(value = "/dept/new", method = "GET")
     public void create() {
-        System.out.println("Methode create() : creation d'un nouveau departement.");
+        System.out.println("GET create()");
+    }
+
+    @URLMapping(value = "/dept/new", method = "POST")
+    public void save() {
+        System.out.println("POST save() - meme URL, methode differente !");
     }
 
     @URLMapping("/dept/list")
     public void list() {
-        System.out.println("Methode list() : liste des departements.");
+        System.out.println("GET list()");
     }
 }
