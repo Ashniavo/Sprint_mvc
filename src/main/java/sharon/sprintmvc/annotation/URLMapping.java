039e32c7ba420f5d8f0dcaa9790093d56d474af3
Ashniavo <sharonrazakarivelo@gmail.com>
2026-06-30 11:21:25 +0300

    Ajout du parametre method dans URLMapping pour distinguer GET et POST

--- src/main/java/sharon/sprintmvc/annotation/URLMapping.java
+++ src/main/java/sharon/sprintmvc/annotation/URLMapping.java
@@ -9,5 +9,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface URLMapping {
     String value();
-     String method() default "GET";
+    String method() default "GET";
 }
