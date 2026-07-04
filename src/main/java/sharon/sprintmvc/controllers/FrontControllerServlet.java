2e64d88bd99bb52d795e422948b3c9f33d36184c
Ashniavo <sharonrazakarivelo@gmail.com>
2026-07-04 17:04:21 +0300

    FrontControllerServlet recupere les routes du contexte et affiche le resultat de methode

--- src/main/java/sharon/sprintmvc/controllers/FrontControllerServlet.java
+++ src/main/java/sharon/sprintmvc/controllers/FrontControllerServlet.java
@@ -2,6 +2,7 @@ package sharon.sprintmvc.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import sharon.sprintmvc.annotation.Controller;
-import sharon.sprintmvc.exception.UrlNotFoundException;
 import sharon.sprintmvc.utils.Mapping;
 import sharon.sprintmvc.utils.UrlKey;
 import sharon.sprintmvc.utils.Utils;
@@ -22,91 +22,81 @@ public class FrontControllerServlet extends HttpServlet {
     List<String> listClasses;
     Map<UrlKey, Mapping> urlMapping;
 
+    @SuppressWarnings("unchecked")
     public void init() throws ServletException {
-        String initial = this.getInitParameter("Controller");
+        super.init();
+        urlMapping = (Map<UrlKey, Mapping>) getServletContext()
+                        .getAttribute("routesWithMethod");
+
         try {
-            List<Class<?>> controllers = Utils.loadClasses(initial, Controller.class);
-            listClasses = Utils.intoString(controllers);
-            urlMapping = Utils.buildUrlMapping(controllers);
+            String initial = this.getInitParameter("Controller");
+            listClasses = Utils.intoString(Utils.loadClasses(initial, Controller.class));
         } catch (Exception e) {
             e.printStackTrace();
             listClasses = null;
-            urlMapping = null;
         }
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
         processRequest(req, res, "GET");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
         processRequest(req, res, "POST");
     }
 
     private void processRequest(HttpServletRequest req, HttpServletResponse res, String httpMethod)
-            throws ServletException, IOException {
+            throws IOException {
 
         res.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = res.getWriter();
 
-        String path = req.getPathInfo();
-        if (path == null) {
-            path = req.getServletPath();
+        try (PrintWriter out = res.getWriter()) {
+            out.println("--- Sprint MVC ---");
+
+            String path = req.getRequestURI().substring(req.getContextPath().length());
+
+            // Nettoyer le slash final
+            if (path.length() > 1 && path.endsWith("/")) {
+                path = path.substring(0, path.length() - 1);
+            }
+
+            UrlKey key = new UrlKey(path, httpMethod);
+
+            if (urlMapping != null && urlMapping.containsKey(key)) {
+                Mapping mapping = urlMapping.get(key);
+                out.println("Route trouvee : " + key + " -> " + mapping);
+
+                try {
+                    Object controller = mapping.getControllerClass()
+                                               .getDeclaredConstructor()
+                                               .newInstance();
+                    Method method = mapping.getMethod();
+                    Object result = method.invoke(controller);
+
+                    if (result != null) {
+                        out.println("Resultat de la methode :");
+                        out.println(result);
+                    }
+
+                } catch (InstantiationException | IllegalAccessException |
+                         InvocationTargetException | NoSuchMethodException e) {
+                    throw new RuntimeException(
+                        "Impossible d'executer la methode liee a " + key, e);
+                }
+
+            } else {
+                out.println("Aucune route trouvee pour l'URL : " + path);
+                if (urlMapping != null) {
+                    urlMapping.forEach((k, v) -> {
+                        out.println(k + " -> " + v.getControllerClass().getName()
+                                    + "->" + v.getMethod().getName() + "()");
+                    });
+                }
+            }
         }
-
-        // Nettoyer le slash final
-        if (path.length() > 1 && path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
-
-        out.println("<!DOCTYPE html>");
-        out.println("<html><head><title>Sprint MVC</title></head><body>");
-        out.println("<h2>URL demandee :</h2>");
-        out.println("<p style='font-size:1.5em; color:#2a6;'>" + path + " [" + httpMethod + "]</p>");
-
-        try {
-            executeMapping(path, httpMethod, out);
-        } catch (UrlNotFoundException e) {
-            out.println("<h2 style='color:red;'>Erreur :</h2>");
-            out.println("<p style='color:red;'>" + e.getMessage() + "</p>");
-            out.println("<h3>URLs connues :</h3>");
-            printKnownUrls(out);
-        }
-
-        out.println("</body></html>");
-    }
-
-    private void executeMapping(String url, String httpMethod, PrintWriter out) throws UrlNotFoundException {
-        UrlKey key = new UrlKey(url, httpMethod);
-
-        if (urlMapping == null || !urlMapping.containsKey(key)) {
-            throw new UrlNotFoundException(url + " [" + httpMethod + "]");
-        }
-
-        Mapping mapping = urlMapping.get(key);
-        try {
-            Object controllerInstance = mapping.getControllerClass().getDeclaredConstructor().newInstance();
-            Method method = mapping.getMethod();
-            method.invoke(controllerInstance);
-
-            out.println("<h2>Resultat :</h2>");
-            out.println("<p>Methode executee : <b>" + mapping + "</b></p>");
-        } catch (Exception e) {
-            out.println("<p style='color:red;'>Erreur lors de l'execution de la methode : " + e.getMessage() + "</p>");
-        }
-    }
-
-    private void printKnownUrls(PrintWriter out) {
-        if (urlMapping == null || urlMapping.isEmpty()) {
-            out.println("<p>Aucune URL connue.</p>");
-            return;
-        }
-        out.println("<ul>");
-        for (Map.Entry<UrlKey, Mapping> entry : urlMapping.entrySet()) {
-            out.println("<li>" + entry.getKey() + " --&gt; " + entry.getValue() + "</li>");
-        }
-        out.println("</ul>");
     }
 }
