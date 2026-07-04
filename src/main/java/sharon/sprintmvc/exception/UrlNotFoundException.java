16af8564c0520d02293f217cc6805d97faf10da1
Ashniavo <sharonrazakarivelo@gmail.com>
2026-07-04 17:04:21 +0300

    Suppression UrlNotFoundException remplacee par RuntimeException comme mini_framework

--- src/main/java/sharon/sprintmvc/exception/UrlNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package sharon.sprintmvc.exception;
-
-public class UrlNotFoundException extends Exception {
-    public UrlNotFoundException(String url) {
-        super("URL non trouvee : " + url);
-    }
-}
