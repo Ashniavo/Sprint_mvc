0af2fbb84a668ba88df243e2fdbc83df16b99730
Ashniavo <sharonrazakarivelo@gmail.com>
2026-07-04 17:04:21 +0300

    Remplace scanner manuel par ClassGraph avec detection de conflit de routes

--- src/main/java/sharon/sprintmvc/utils/Utils.java
+++ src/main/java/sharon/sprintmvc/utils/Utils.java
@@ -1,48 +1,52 @@
 package sharon.sprintmvc.utils;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
+
 import sharon.sprintmvc.annotation.URLMapping;
-import sharon.sprintmvc.utils.UrlKey;
+
 public class Utils {
 
-    public static List<Class<?>> loadClasses(String packageName, Class<? extends Annotation> annotation) throws Exception {
-        List<Class<?>> result = new ArrayList<>();
-        String path = packageName.replace('.', '/');
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> resources = classLoader.getResources(path);
+    private static Properties loadConfig() {
+        Properties prop = new Properties();
+        try (InputStream input = Utils.class.getClassLoader()
+                                            .getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("config.properties introuvable !");
+            }
+            prop.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Erreur lecture config.properties", e);
+        }
+        return prop;
+    }
 
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            File directory = new File(resource.getFile());
-            if (directory.exists() && directory.isDirectory()) {
-                findClasses(directory, packageName, annotation, result);
+    public static List<Class<?>> loadClasses(String packageName,
+                                              Class<? extends Annotation> annotation)
+            throws Exception {
+        List<Class<?>> result = new ArrayList<>();
+        try (ScanResult scanResult = new ClassGraph()
+                .enableAllInfo()
+                .acceptPackages(packageName)
+                .scan()) {
+            ClassInfoList classes = scanResult.getClassesWithAnnotation(annotation.getName());
+            for (ClassInfo classInfo : classes) {
+                result.add(Class.forName(classInfo.getName()));
             }
         }
         return result;
-    }
-
-    private static void findClasses(File directory, String packageName, Class<? extends Annotation> annotation, List<Class<?>> result) throws ClassNotFoundException {
-        File[] files = directory.listFiles();
-        if (files == null) return;
-        for (File file : files) {
-            if (file.isDirectory()) {
-                findClasses(file, packageName + "." + file.getName(), annotation, result);
-            } else if (file.getName().endsWith(".class")) {
-                String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
-                Class<?> clazz = Class.forName(className);
-                if (clazz.isAnnotationPresent(annotation)) {
-                    result.add(clazz);
-                }
-            }
-        }
     }
 
     public static List<String> intoString(List<Class<?>> classes) {
@@ -53,17 +57,45 @@ public class Utils {
         return names;
     }
 
-    public static Map<UrlKey, Mapping> buildUrlMapping(List<Class<?>> controllers) {
-    Map<UrlKey, Mapping> urlMap = new HashMap<>();
-    for (Class<?> controllerClass : controllers) {
-        for (Method method : controllerClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(URLMapping.class)) {
-                URLMapping urlMapping = method.getAnnotation(URLMapping.class);
-                UrlKey key = new UrlKey(urlMapping.value(), urlMapping.method());
-                urlMap.put(key, new Mapping(controllerClass, method));
+    public static Map<UrlKey, Mapping> buildUrlMapping(List<Class<?>> controllers)
+            throws IllegalStateException {
+        Map<UrlKey, Mapping> urlMap = new HashMap<>();
+        for (Class<?> controllerClass : controllers) {
+            for (Method method : controllerClass.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(URLMapping.class)) {
+                    URLMapping urlMapping = method.getAnnotation(URLMapping.class);
+                    UrlKey key = new UrlKey(urlMapping.value(), urlMapping.method());
+
+                    // Detection de conflit
+                    if (urlMap.containsKey(key)) {
+                        Mapping existing = urlMap.get(key);
+                        throw new IllegalStateException(
+                            "[ERREUR] Conflit de routes detecte !\n" +
+                            "La route " + key + " est deja associee a : " +
+                            existing.getControllerClass().getName() + "." +
+                            existing.getMethod().getName() + "()\n" +
+                            "Impossible de la reassigner a : " +
+                            controllerClass.getName() + "." + method.getName() + "()"
+                        );
+                    }
+
+                    urlMap.put(key, new Mapping(controllerClass, method));
+                }
             }
         }
+        return urlMap;
     }
-    return urlMap;
-}
+
+    public static Map<UrlKey, Mapping> buildUrlMappingFromConfig() throws Exception {
+        Properties prop = loadConfig();
+        String packageName = prop.getProperty("app.package");
+
+        @SuppressWarnings("unchecked")
+        Class<? extends Annotation> controllerAnnotation =
+            (Class<? extends Annotation>) Class.forName(
+                prop.getProperty("annotation.controller"));
+
+        List<Class<?>> controllers = loadClasses(packageName, controllerAnnotation);
+        return buildUrlMapping(controllers);
+    }
 }
