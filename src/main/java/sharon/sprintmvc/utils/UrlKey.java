5769c94fb5d626ce27ea66d66e40b233a6992545
Ashniavo <sharonrazakarivelo@gmail.com>
2026-07-04 17:04:21 +0300

    Ameliore UrlKey avec Objects.hash pour un hashCode plus robuste

--- src/main/java/sharon/sprintmvc/utils/UrlKey.java
+++ src/main/java/sharon/sprintmvc/utils/UrlKey.java
@@ -1,38 +1,42 @@
 package sharon.sprintmvc.utils;
 
+import java.util.Objects;
+
 public class UrlKey {
 
-    private String url;
-    private String httpMethod;
+    private final String url;
+    private final String httpMethod;
 
     public UrlKey(String url, String httpMethod) {
         this.url = url;
         this.httpMethod = httpMethod;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getHttpMethod() {
-        return httpMethod;
-    }
+    public String getUrl() { return url; }
+    public String getHttpMethod() { return httpMethod; }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (!(obj instanceof UrlKey)) return false;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
         UrlKey other = (UrlKey) obj;
-        return this.url.equals(other.url) && this.httpMethod.equals(other.httpMethod);
+        if (url == null) {
+            if (other.url != null) return false;
+        } else if (!url.equals(other.url)) return false;
+        if (httpMethod == null) {
+            if (other.httpMethod != null) return false;
+        } else if (!httpMethod.equals(other.httpMethod)) return false;
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return (url + httpMethod).hashCode();
+        return Objects.hash(url, httpMethod);
     }
 
     @Override
     public String toString() {
-        return url + " [" + httpMethod + "]";
+        return "[url: " + url + ", method : " + httpMethod + "]";
     }
 }
