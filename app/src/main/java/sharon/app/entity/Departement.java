d5cd92d6c2da3cd669f0c42355726996ef65a97b
Ashniavo <sharonrazakarivelo@gmail.com>
2026-07-18 20:44:59 +0300

    Sprint5bis ajout de l entite Departement

---
+++ app/src/main/java/sharon/app/entity/Departement.java
@@ -0,0 +1,17 @@
+package sharon.app.entity;
+
+public class Departement {
+    private int id;
+    private String nom;
+
+    public Departement(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+
+    @Override
+    public String toString() { return nom; }
+}
